0b95aadf810de018b4c71cac8cab5f8ea8b44d33
wanjiku <wanjikukangangi@gmail.com>
2023-07-05 16:09:47 +0300

    update ussd

--- ussd/models.py
+++ ussd/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    eventName = models.CharField(max_length=100)
-    organizer = models.CharField(max_length=100)
-    frequency:models.IntegerField(default=30)
+    eventName = models.CharField(max_length=100, null=True, blank=True)
+    organizer = models.CharField(max_length=100, null=True, blank=True)
+    frequency = models.IntegerField(default=30, null=True, blank=True)
 
     def _str_(self):
         return self.name
 
 class User(models.Model):
-    firstName=models.CharField(max_length=100)
-    lastName=models.CharField(max_length=100)
-    email=models.CharField(max_length=100)
-    phoneNumber=models.CharField(max_length=100)
+    firstName=models.CharField(max_length=100, null=True, blank=True)
+    lastName=models.CharField(max_length=100, null=True, blank=True)
+    email=models.CharField(max_length=100, null=True, blank=True)
+    phoneNumber=models.CharField(max_length=100, null=True, blank=True)
 
     def _str_(self):
         return self.name
@@ -21,7 +21,7 @@ class User(models.Model):
 class Subscriptions(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    nextReminder=models.DateField()
+    nextReminder=models.DateField(null=True, blank=True)
     startDate = models.DateField()
     endDate = models.DateField()
 
--- ussd/views.py
+++ ussd/views.py
@@ -2,6 +2,7 @@ import datetime
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from dateutil import relativedelta
+from ussd.models import User, Event, Subscriptions
 
 @csrf_exempt
 def index(request):
@@ -14,8 +15,7 @@ def index(request):
         print(text)
         print("*"*80)
 
-        # Confirm if user exists
-        # user = User.objects.filter(phone_number=phone_number)
+        user = User.objects.get_or_create(phone_number=phone_number)[0]
 
         response = ""
 
@@ -26,6 +26,7 @@ def index(request):
 
         elif text == "1":
             #Get event details
+            event = Event.objects.get_or_create(eventName="AT Hackathon")[0]
             response = "CON AT Hackathon next on event-date. Would you like to rsvp? \n"
             response += "1. Yes \n"
             response += "2. No \n"
@@ -44,21 +45,37 @@ def index(request):
         elif text == "1*1*1":
             date = datetime.date.today()
             end_date = date.replace(year=date.year + 1)
-            #create subcription f
+            event = Event.objects.get_or_create(eventName="AT Hackathon")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
 
             response = f"END Thank you for rsvping for AT Hackathon. Your subcription will end {end_date}"
 
         elif text == "1*1*2":
             date = datetime.date.today()
             end_date = date +  relativedelta.relativedelta(months=6) 
-            #create subcription
+            event = Event.objects.get_or_create(eventName="AT Hackathon")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
+
 
             response = f"END Thank you for rsvping for AT Hackathon. Your subcription will end {end_date}"
 
         elif text == "1*1*3":
             date = datetime.date.today()
             end_date = date +  relativedelta.relativedelta(months=3) 
-            #create subcription
+            event = Event.objects.get_or_create(eventName="AT Hackathon")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
 
             response = f"END Thank you for rsvping for AT Hackathon. Your subcription will end {end_date}"
 
@@ -86,21 +103,36 @@ def index(request):
         elif text == "2*1*1":
             date = datetime.date.today()
             end_date = date.replace(year=date.year + 1)
-            #create subcription f
+            event = Event.objects.get_or_create(eventName="WIT Event")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
 
             response = f"END Thank you for rsvping for WIT Event. Your subcription will end {end_date}"
 
         elif text == "2*1*2":
             date = datetime.date.today()
             end_date = date +  relativedelta.relativedelta(months=6) 
-            #create subcription
+            event = Event.objects.get_or_create(eventName="WIT Event")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
 
             response = f"END Thank you for rsvping for WIT Event. Your subcription will end {end_date}"
 
         elif text == "2*1*3":
             date = datetime.date.today()
             end_date = date +  relativedelta.relativedelta(months=3) 
-            #create subcription
+            event = Event.objects.get_or_create(eventName="WIT Event")[0]
+            subscription = Subscriptions.objects.create(
+                event=event,
+                startDate=date,
+                endDate=end_date
+            )
 
             response = f"END Thank you for rsvping for AT Hackathon. Your subcription will end {end_date}"
 
